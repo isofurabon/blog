35183f4f7df31964ab347ee3c5ff4933662d3704
isofurabon <isofurabon@users.noreply.github.com>
2020-12-13 06:38:44 +0000

    feat: execute command with env variables

--- build/actions/build-and-deploy/entrypoint.sh
+++ build/actions/build-and-deploy/entrypoint.sh
@@ -1,6 +1,3 @@
-#!/bin/sh -eux
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
-
+#!/bin/sh -eu
+echo "retrive & generate posts"
 python3 build/actions/build-and-deploy/retrive_and_generate_posts.py
